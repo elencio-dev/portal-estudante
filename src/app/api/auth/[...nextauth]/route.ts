fd80adb9d2dde0e23624d231aff2ccdca5b7e48b
Elencio <elenciocalado@gmail.com>
2024-09-01 12:33:18 -0300

    verificando a autenticacao

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,18 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/lib/AuthOptions";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = NextAuth(authOptions);
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await NextAuth(req, res, authOptions);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(504).json({ message: "An error occurred: " + error.message });
+    } else {
+      res.status(504).json({ message: "An unknown error occurred" });
+    }
+  }
+};
 
 export { handler as GET, handler as POST };
