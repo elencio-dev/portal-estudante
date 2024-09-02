e77e24611ba79b6b7a8ca2f4e3c1068cc276efa5
Elencio <elenciocalado@gmail.com>
2024-09-02 12:40:11 -0300

    chore: Melhorando o Loading

--- src/app/(Dashboard)/Home/page.tsx
+++ src/app/(Dashboard)/Home/page.tsx
@@ -54,7 +54,7 @@ export default function Dashboard() {
   if (status === "loading") {
     return (
       <div className="flex justify-center items-center h-screen">
-        <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+        <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12"></div>
       </div>
     );
   }
